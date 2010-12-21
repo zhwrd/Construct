0e9dcf461bd638c7fbc611e5af5e1c0bbd5f9eb9
macrosound <howard.zach@gmail.com>
2010-12-20 22:28:01 -0500

    added Wire class

--- src/core/wire.cpp
+++ src/core/wire.cpp
@@ -1,7 +1,65 @@
 #include "wire.h"
+#include <core/signal_socket.h>
+#include <cassert>
 
 namespace construct {
 namespace core {
+
+Wire::Wire() :  volume_(1.0), pan_(0.0), r_volume_(1.0), l_volume_(1.0),
+                source_(NULL), destination_(NULL), buffer_(NULL) {}
+               
+Wire::~Wire() {
+  if (source_) Disconnect(source_);
+  if (destination_) Disconnect(destination_);
+}
+  
+void Wire::Connect(SignalSocket* source, SignalSocket* destination) {
+  source_ = source;
+  destination_ = destination;
+  source_->Connect(this);
+  destination_->Connect(this);
+}
+
+void Wire::ChangeSource(SignalSocket* source) {
+  assert(source_);
+  Disconnect(source_);
+  source_ = source;
+  source_->Connect(this);
+}
+
+void Wire::ChangeDestination(SignalSocket* destination) {
+  assert(destination_);
+  Disconnect(destination_);
+  destination_ = destination;
+  destination_->Connect(this);
+}
+
+void Wire::CollectData(int num_samples) {
+  source_->CollectData(num_samples);
+}
+
+void Wire::SetVolume(double volume) {
+  volume_ = volume;
+  if (pan_ > 0.0) {
+    r_volume_ = volume;
+    l_volume_ = r_volume_*(1.0 - pan_);
+  } else {
+    l_volume_ = volume;
+    r_volume_ = l_volume_*(1.0 + pan_);
+  }
+}
+
+void Wire::SetPan(double pan) {
+  pan_ = pan;
+  SetVolume(volume_);
+}
+
+void Wire::Disconnect(SignalSocket* signalsocket) {
+  if (signalsocket == source_) { source_ = NULL; }
+  else if (signalsocket == destination_) { destination_ = NULL; }
+  else { return; }
+  signalsocket->Disconnect(this);
+}
 
 } // namespace core
 } // namespace construct
--- src/core/wire.h
+++ src/core/wire.h
@@ -4,8 +4,34 @@
 namespace construct {
 namespace core {
 
-class Wire {
+class SignalSocket;
+class SignalBuffer;
 
+class Wire {
+ public:
+  Wire();
+  ~Wire();
+  
+  virtual void Connect(SignalSocket* source, SignalSocket* destination);
+  virtual void ChangeSource(SignalSocket* source);
+  virtual void ChangeDestination(SignalSocket* destination);
+  virtual void CollectData(int num_samples);
+  virtual void SetVolume(double volume);
+  virtual void SetPan(double pan);
+
+  virtual inline SignalBuffer* get_buffer() { return buffer_; }
+  virtual inline void set_buffer(SignalBuffer* buffer) { buffer_ = buffer; }
+
+ protected:
+  virtual void Disconnect(SignalSocket* signalsocket);
+
+  double volume_;
+  double pan_;
+  double r_volume_;
+  double l_volume_;
+  SignalSocket* source_;
+  SignalSocket* destination_;
+  SignalBuffer* buffer_;
 };
 
 } // namespace core
