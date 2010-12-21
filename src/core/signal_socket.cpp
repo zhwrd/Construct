34ba6df117de114700f82920a14e783b68674e3a
macrosound <howard.zach@gmail.com>
2010-12-20 22:41:07 -0500

    finished implementing SignalSocket

--- src/core/signal_socket.cpp
+++ src/core/signal_socket.cpp
@@ -1,7 +1,11 @@
 #include "signal_socket.h"
+#include <algorithm>
+#include <cassert>
 
 namespace construct {
 namespace core {
+ 
+// SignalSocket
 
 SignalSocket::SignalSocket(UnitGenerator& unitgenerator, std::string name)
   : unitgenerator_(unitgenerator), name_(name), signalbuffer_(NULL) {}
@@ -9,11 +13,29 @@ SignalSocket::SignalSocket(UnitGenerator& unitgenerator, std::string name)
 SignalSocket::~SignalSocket() {}
 
 void SignalSocket::Connect(Wire* wire) {
-
+  wires_.push_back(wire);
 }
 
 void SignalSocket::Disconnect(Wire* wire) {
+  std::vector<Wire*>::iterator i(std::find(wires_.begin(), wires_.end(), wire));
+  assert(i != wires_.end());
+  wires_.erase(i);
+}
 
+// InputSocket
+
+void InputSocket::CollectData(int num_samples) {
+  for ( std::vector<Wire*>::iterator i = wires_.begin();
+        i != wires_.end();
+        ++i) {
+    (*i)->CollectData(num_samples);
+  }
+}
+
+// OutputSocket
+
+void OutputSocket::CollectData(int /* num_samples */) {
+  // Does nothing
 }
 
 } // namespace core
--- src/core/signal_socket.h
+++ src/core/signal_socket.h
@@ -5,6 +5,7 @@
 #include <core/unit_generator.h>
 #include <core/wire.h>
 #include <string>
+#include <vector>
 
 namespace construct {
 namespace core {
@@ -28,13 +29,24 @@ class SignalSocket {
     signalbuffer_ = signalbuffer;
   }
   virtual inline const std::string& get_name() const { return name_; }
-  virtual inline void set_name(const std::string& name) { this->name_ = name; }
+  virtual inline void set_name(const std::string& name) { name_ = name; }
 
- private:
+ protected:
   UnitGenerator& unitgenerator_;
   std::string name_;
   SignalBuffer* signalbuffer_;
+  std::vector<Wire*> wires_;
 
+};
+
+class InputSocket : public SignalSocket {
+ public:
+  virtual void CollectData(int num_samples);  
+};
+
+class OutputSocket : public SignalSocket {
+ public:
+  virtual void CollectData(int num_samples);  
 };
 
 } // namespace core
